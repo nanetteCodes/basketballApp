3411502aa74b3db3b75fa216403fe82b0fff0f1e
Nanette <njulius89@gmail.com>
2020-02-18 21:40:48 -0800

    comment server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -9,13 +10,12 @@ connectDB();
 // INIT MIDDLEWARE
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
-
 // DEFINE ROUTES
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
 
 const PORT = process.env.PORT || 5000;
 
