9b2c5c179724cf10d22374d295b6c0c0389ed073
Nanette <njulius89@gmail.com>
2020-02-19 09:58:06 -0800

    fixed mongouri

--- config/db.js
+++ config/db.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const dotenv = require('dotenv')
-dotenv.config()
 
 // get from default.json
 const db = process.env.mongoURI;
